87214ca80fa152b9946eb3e504dfd296ad396ced
LeoDNascimento <leonardo.nascimento1306@gmail.com>
2020-07-14 05:53:03 -0300

    Post finalizado e listagem

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,13 @@ interface Balance {
   total: number;
 }
 
+// eslint-disable-next-line @typescript-eslint/class-name-casing
+interface transactionsCTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +21,18 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+  // public getBalance(): Balance {
+  //   // TODO
+  // }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: transactionsCTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionsService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+/**
+ * [x] Uma rota post que receba 'title, value e type' no body
+ *     ° Type é o tipo da transação = income (depósitos) e outcome (retiradas)
+ * [x] Criar uma rota get para listar todas as transações cadastradas
+ * [ ] Retornar junto do valor um balanço
+ *     ° Soma das entradas, retiradas e total restante
+ */
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    return response.json(transactions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +26,18 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionsService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
